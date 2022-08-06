fbbaaf4603153a18b06df04dc24e8d009a148970
Daniil <89664655+ucrdaniel@users.noreply.github.com>
2022-08-06 11:57:34 -0300

    Add files via upload

--- library/authors/views.py
+++ library/authors/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.renderers import JSONRenderer
+
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Author
+from .serializers import AuthorModelSerializer
+
+class AuthorModelViewSet(ModelViewSet):
+    # renderer_classes = [JSONRenderer]
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
