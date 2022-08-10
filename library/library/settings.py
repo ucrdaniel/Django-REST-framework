d58a4c52fe01352604a11e1b57fcd38d419b7923
Daniil <89664655+ucrdaniel@users.noreply.github.com>
2022-08-10 13:14:08 -0300

    Add files via upload

--- library/library/settings.py
+++ library/library/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+CORS_ALLOWED_ORIGINS =[
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,11 +44,13 @@ INSTALLED_APPS = [
     #main
     'rest_framework',
     'authors',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
