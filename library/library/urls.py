f7a3c3a8fbc773562230071f0b955da00c5bb220
Daniil <89664655+ucrdaniel@users.noreply.github.com>
2022-08-15 10:53:47 -0300

    Add files via upload

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,7 +1,7 @@
 """library URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter,SimpleRouter
+
+from authors.views import AuthorModelViewSet,BookModelViewSet,BiographyModelViewSet
+
+router = SimpleRouter()
+router.register('authors',AuthorModelViewSet)
+router.register('books',BookModelViewSet)
+router.register('biographies',BiographyModelViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
