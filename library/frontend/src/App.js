17f3de320b2570b731294cbbeb133c0f230bd533
Daniil <89664655+ucrdaniel@users.noreply.github.com>
2022-08-26 16:04:14 -0300

    Add files via upload

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -4,8 +4,17 @@ import AuthorList from "./components/Author";
 import BookList from "./components/Books";
 import NotFound404 from "./components/NotFound404";
 import BooksAuthor from "./components/BooksAuthor";
+import LoginForm from "./components/Auth";
 import axios from "axios";
-import {BrowserRouter, Route, Routes, Link, Navigate, useLocation} from 'react-router-dom'
+import Cookies from "universal-cookie";
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+    Link,
+    Navigate,
+    useLocation
+} from 'react-router-dom'
 
 
 class App extends React.Component {
@@ -13,13 +22,40 @@ class App extends React.Component {
         super(props)
         this.state = {
             'authors': [],
-            'books': []
+            'books': [],
+            'token': '',
         }
     }
 
+    logout(){
+        this.set_token('')
+    }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8010/api/authors/').then(response => {
+    is_auth(){
+        return !!this.state.token
+    }
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, () => this.load_data())
+    }
+    get_token_from_storage(){
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, () => this.load_data())
+    }
+
+    get_token(username, password) {
+        const data = {username: username, password: password}
+        axios.post('http://127.0.0.1:8010/api-token-auth/', data).then(response => {
+            this.set_token(response.data['token'])
+        }).catch(error => alert('Неверный логин или пароль'))
+    }
+
+
+    load_data() {
+        const  headers = this.get_headers()
+        axios.get('http://127.0.0.1:8010/api/authors/',{headers}).then(response => {
 
             this.setState(
                 {
@@ -29,7 +65,7 @@ class App extends React.Component {
         }).catch(error => console.log(error))
 
 
-        axios.get('http://127.0.0.1:8010/api/books/').then(response => {
+        axios.get('http://127.0.0.1:8010/api/books/',{headers}).then(response => {
 
             this.setState(
                 {
@@ -39,24 +75,48 @@ class App extends React.Component {
         }).catch(error => console.log(error))
     }
 
+    get_headers(){
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_auth()){
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+
+    }
+    componentDidMount() {
+        this.get_token_from_storage()
+    }
+
 
     render() {
         return (
             <div>
                 <BrowserRouter>
                     <nav>
-                        <li> <Link to='/'>Authors</Link> </li>
-                        <li> <Link to='/books'>Books</Link> </li>
+                        <li><Link to='/'>Authors</Link></li>
+                        <li><Link to='/books'>Books</Link></li>
+                        <li>
+                            {this.is_auth() ? <button onClick={()=> this.logout()}> Logout </button> : <Link to='/login'>Login</Link>}
+                        </li>
                     </nav>
 
                     <Routes>
-                        <Route exact path='/' element={<Navigate to='/authors' />} />
-                        <Route exact path='/books' element={<BookList books={this.state.books} />} />
+                        <Route exact path='/'
+                               element={<Navigate to='/authors'/>}/>
+                        <Route exact path='/books'
+                               element={<BookList books={this.state.books}/>}/>
+                        <Route exact path='/login' element={<LoginForm
+                            get_token={(username, password) => this.get_token(username, password)}/>}/>
+
                         <Route path='/authors'>
-                            <Route index element={<AuthorList authors={this.state.authors} />} />
-                            <Route path=':authorId' element={<BooksAuthor books={this.state.books} />} />
+                            <Route index element={<AuthorList
+                                authors={this.state.authors}/>}/>
+                            <Route path=':authorId' element={<BooksAuthor
+                                books={this.state.books}/>}/>
                         </Route>
-                        <Route path='*' element={<NotFound404 />} />
+                        <Route path='*' element={<NotFound404/>}/>
                     </Routes>
                 </BrowserRouter>
             </div>
